a6757fd6aac20ebab79d13225fc3bccb0bc7bb31
DeceptiveRat <0101087ssk@gmail.com>
2026-02-06 09:05:31 +0900

    changes: 1. added streaming mode to timelapse script. 2. modified help screen to be more precise for timelapse script.

--- timelapse/timelapse.py
+++ timelapse/timelapse.py
@@ -4,29 +4,31 @@ import subprocess
 import sys
 import getopt
 import time
-import glob
+from mss import mss
 
 def usage():
 	print("usage:", sys.argv[0])
 	print("options:")
-	print("-h: display this help screen")
-	print("-f: set fps. Default: 5")
-	print("-l: set screenshot save location. Default: . Use with -d option")
-	print("-b: set bitrate. Default: 10 Mbps")
-	print("-c: set capture fps. Default: 5. Recommended to be less than fps")
-	print("-o: set output file name. Default: timelapse.mp4")
-	print("-d: do not delete screenshots")
+	print("\t-h: display this help screen")
+	print("\t-f [integer]: set fps. Default: 7")
+	print("\t-l [save location]: set screenshot save location. Irrelevant without -s option. Default: .")
+	print("\t-b [integer][k/M]: set bitrate. Default: 500k")
+	print("\t-c [integer]: set capture fps. Recommended to be less than fps. Default: 2")
+	print("\t-o [filename]: set output file name. Default: timelapse.mp4")
+	print("\t-s: save screenshots. Runs ffmpeg in static mode")
+	print("\t-r [resolution]: set screen resolution. Irrelevant when using -s option. Default 1920x1080")
 
-fps=5
-capture_fps=5
-bitrate=10
+fps=7
+capture_fps=2
+bitrate="500k"
 save_location="."
 count=1
 output_file="timelapse.mp4"
 delete_screenshots=True
+screen_resolution="1920x1080"
 
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "hf:l:b:c:o:d")
+	opts, args = getopt.getopt(sys.argv[1:], "hf:l:b:c:o:sr:")
 except getopt.GetoptError as err:
 	print(err)
 	usage()
@@ -41,34 +43,78 @@ for option, argument in opts:
 	elif option == "-l":
 		save_location = argument
 	elif option == "-b":
-		bitrate = int(argument)
+		bitrate = argument
 	elif option == "-c":
 		capture_fps = int(argument)
 	elif option == "-o":
 		output_file = argument
-	elif option == "-d":
+	elif option == "-s":
 		delete_screenshots=False
+	elif option == "-r":
+		screen_resolution=argument
 	else:
 		assert False, "unhandled option"
 
-try:
-	while True:
-		time.sleep(1/capture_fps)
-		file_name = save_location+'/'+f"frame{count:06d}.png"
-		subprocess.run(['flameshot', 'full', '-p', file_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-		print("saved")
-		count+=1
-except KeyboardInterrupt:
+wait_time = 1/capture_fps
+
+if not delete_screenshots:
+	try:
+		while True:
+			print("Recording... Press Ctrl+C to save and exit.")
+			time.sleep(wait_time)
+			file_name = save_location+'/'+f"frame{count:06d}.png"
+			subprocess.run(['flameshot', 'full', '-p', file_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+			print("\rframes captured: %d" %(count), end="")
+			count+=1
+	except KeyboardInterrupt:
+		print()
+		print("Keyboard interrupt detected. Exiting capture.")
+		
+	print("Creating video from images...")
+	file_name = save_location+'/frame%06d.png'
+	subprocess.call(['ffmpeg', '-framerate', str(fps), '-i', file_name, '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-b:v', bitrate, output_file])
 	print()
-	print("Keyboard interrupt detected. Exiting capture.")
-	
-print("Creating video from images...")
-file_name = save_location+'/frame%06d.png'
-subprocess.call(['ffmpeg', '-framerate', str(fps), '-i', file_name, '-c:v', 'libx264', '-pix_fmt', 'yuv420p', output_file])
-print()
-if delete_screenshots:
-	print("Deleting images...")
-	files = glob.glob(save_location+'/frame*.png')
-	if files:
-		subprocess.call(['rm', *files])
-print("Terminating...")
+	print("Terminating...")
+
+else:
+	ffmpeg_command = [
+		'ffmpeg',
+		'-y', # overwrite output file without asking
+		'-f', 'rawvideo', # format rawvideo
+		'-c:v', 'rawvideo', # input is rawvideo 
+		'-s', screen_resolution, # set frame size
+		'-pix_fmt', 'bgra', # set pixel format
+		'-r', str(fps), # set frame rate
+		'-i', '-', # use stdin as input
+		'-c:v', 'libx264', # output codec is h.264
+		'-pix_fmt', 'yuv420p', # set pixel format
+		'-b:v', bitrate,
+		output_file
+	]
+
+	# open subprocess with pipe to stdin open
+	process = subprocess.Popen(ffmpeg_command, stdin=subprocess.PIPE, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
+	try:
+		with mss() as sct:
+			# set monitor to main monitor
+			monitor = sct.monitors[1]
+			print("Recording... Press Ctrl+C to save and exit.")
+			
+			while True:
+				# Capture and write immediately
+				img = sct.grab(monitor)
+				process.stdin.write(img.raw)
+
+				print("\rframes captured: %d" %(count), end="")
+				time.sleep(wait_time)
+				count+=1
+
+	except KeyboardInterrupt:
+		print()
+		print("Keyboard interrupt detected. Exiting capture.")
+	finally:
+		# properly close subprocess
+		process.stdin.close()
+		process.wait()
+		print("Terminating...")
