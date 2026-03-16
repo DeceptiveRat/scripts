5af5d136ebb2ee9ee1fa6102a10ad82ec983cefa
DeceptiveRat <0101087ssk@gmail.com>
2026-03-16 18:03:25 +0900

    changed get byte offset to read data in chunks

--- get_byte_offset/get_byte_offset.py
+++ get_byte_offset/get_byte_offset.py
@@ -9,17 +9,20 @@ def usage():
 	print("options:")
 	print("    -h: display this help screen")
 	print("    -f <file>: file to search. If this is not provided, stdin is used")
-	print("    -l <int>: search length. Default: 1,000,000 bytes")
-	print("    -p <pattern>: [required] pattern to search for. Enter as hex; e.g. 0x3ccf10")
-	print("    -s: switch endianness. Default: False")
+	print("    -p <pattern>: [required] pattern to search for. Enter as hex; e.g. 3ccf10")
+	print("    -e: switch endianness. Default: False")
+	print("    -s <int>: skip bytes")
+	print("    -l <int>: max length to parse in bytes. Skipped bytes are included. Default: 100,000,000")
 
 file_name = ""
-search_length=1000000
+chunk_length=1000000
+max_search_length = 100000000
 search_pattern=""
+skip_length = 0
 switch_endianness=False
 
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "hf:l:p:s")
+	opts, args = getopt.getopt(sys.argv[1:], "hf:l:p:es:")
 except getopt.GetoptError as err:
 	print(err)
 	usage()
@@ -35,31 +38,65 @@ for option, argument in opts:
 		search_length=int(argument)
 	elif option == "-p":
 		search_pattern = argument
-	elif option == "-s":
+	elif option == "-e":
 		switch_endianness = True
+	elif option == "-s":
+		skip_length = int(argument)
 	else:
 		assert False, "unhandled option"
 
+if search_pattern == "":
+	print("Error! Please enter search pattern")
+	exit(-1)
+
+remaining_bytes = max_search_length - skip_length
+read_length = chunk_length
+
+# read only remaining bytes
+if remaining_bytes < chunk_length:
+	read_length = remaining_bytes
+
 if file_name != "":
 	with open(file_name, 'rb') as f:
-		data_array = f.read(search_length)
+		data_array = f.seek(skip_length)
+		data_array = f.read(read_length)
 else:
-	data_array = sys.stdin.buffer.read(search_length)
-pos = 0
+	data_array = sys.stdin.buffer.read(skip_length)
+	data_array = sys.stdin.buffer.read(read_length)
+
+remaining_bytes = remaining_bytes - read_length
+offset = skip_length
+
 if switch_endianness:
 	search_pattern = hex(int.from_bytes(bytes.fromhex(search_pattern)))
 	search_pattern=search_pattern[2:]
 if len(search_pattern)%2 != 0:
 	search_pattern = "0" + search_pattern
-#print(search_pattern)
-#print(type(search_pattern))
 
-while True:
-	# search for pattern
-	pos=data_array.find(bytes.fromhex(search_pattern), pos)
-	if pos == -1:
-		break;
-	# print offset
-	print("offset: %d" %(pos))
-	# search starts from next byte
-	pos+=1
+while data_array != "":
+	pos = 0
+	while True:
+		# search for pattern
+		pos=data_array.find(bytes.fromhex(search_pattern), pos)
+		if pos == -1:
+			break
+		# print offset
+		print("offset: %d" %(offset+pos))
+		# search starts from next byte
+		pos+=1
+	
+	if remaining_bytes == 0:
+		break
+	
+	offset = offset + read_length
+	# read only remaining bytes
+	if remaining_bytes < chunk_length:
+		read_length =remaining_bytes
+
+	if file_name != "":
+		with open(file_name, 'rb') as f:
+			data_array = f.seek(offset)
+			data_array = f.read(read_length)
+	else:
+		data_array = sys.stdin.buffer.read(read_length)
+	remaining_bytes = remaining_bytes - read_length
